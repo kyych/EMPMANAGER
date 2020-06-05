c655619900f4b316bfb7da36b7787f82b0ec58db
kyych <m.biernat43@gmail.com>
2020-06-06 01:47:00 +0200

    added sorting for age, in future I will change to sorting by salary

--- src/front/FilterPanel.java
+++ src/front/FilterPanel.java
@@ -3,16 +3,17 @@ package front;
 import back.EmployeeTableModel;
 
 import javax.swing.*;
+import javax.swing.border.Border;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
 import javax.swing.table.TableRowSorter;
 import java.awt.*;
 
 public class FilterPanel extends JPanel {
-    private JTextField textField = new JTextField();
+    private JTextField searchForAllTextField = new JTextField();
+    private JTextField searchBySalary = new JTextField();
     private MainViewController controller;
     private TableRowSorter<EmployeeTableModel> tableRowSorter; //= new TableRowSorter<>(controller.getjTableSortingExample().getTableModel());
-
 
     public FilterPanel() {
         this.setLayout(new BorderLayout());
@@ -20,15 +21,19 @@ public class FilterPanel extends JPanel {
 
     public void setUI(){
         tableRowSorter = new TableRowSorter<>(controller.getjTableSortingExample().getTableModel());
-        controller.getjTableSortingExample().getTable().setRowSorter(tableRowSorter);
-        this.add(new JLabel("Specify a word to match:"),
-                BorderLayout.WEST);
-        this.add(textField, BorderLayout.CENTER);
 
-        textField.getDocument().addDocumentListener(new DocumentListener() {
+        controller.getjTableSortingExample().getTable().setRowSorter(tableRowSorter);
+//        this.add(new JLabel("Specify a word to match:"),
+//                BorderLayout.WEST);
+        searchForAllTextField.setPreferredSize(new Dimension(350,100));
+        searchBySalary.setPreferredSize(new Dimension(350,100));
+        this.add(searchForAllTextField, BorderLayout.WEST);
+        this.add(searchBySalary, BorderLayout.EAST);
+
+        searchForAllTextField.getDocument().addDocumentListener(new DocumentListener() {
             @Override
             public void insertUpdate(DocumentEvent e) {
-                String text = textField.getText();
+                String text = searchForAllTextField.getText();
 
                 if (text.trim().length() == 0) {
                     tableRowSorter.setRowFilter(null);
@@ -39,7 +44,7 @@ public class FilterPanel extends JPanel {
 
             @Override
             public void removeUpdate(DocumentEvent e) {
-                String text = textField.getText();
+                String text = searchForAllTextField.getText();
 
                 if (text.trim().length() == 0) {
                     tableRowSorter.setRowFilter(null);
@@ -51,6 +56,36 @@ public class FilterPanel extends JPanel {
             @Override
             public void changedUpdate(DocumentEvent e) {
                 throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+        });
+        searchBySalary.getDocument().addDocumentListener(new DocumentListener() {
+            @Override
+            public void insertUpdate(DocumentEvent e) {
+                String text = searchBySalary.getText();
+
+                if (text.trim().length() == 0) {
+                    tableRowSorter.setRowFilter(null);
+                } else {
+                    tableRowSorter.setRowFilter(RowFilter.regexFilter("(?i)"+text, 3)); // TODO: AFTER MODEL BEING UPDATED, CHANGE COLUMN NUMBER (3) TO SALARY COLUMN
+                                                                                                        // 3 is age column, to change
+                }
+            }
+
+            @Override
+            public void removeUpdate(DocumentEvent e) {
+                String text = searchBySalary.getText();
+
+                if (text.trim().length() == 0) {
+                    tableRowSorter.setRowFilter(null);
+                } else {
+                    tableRowSorter.setRowFilter(RowFilter.regexFilter("(?i)" + text,3));
+                }
+            }
+
+            @Override
+            public void changedUpdate(DocumentEvent e) {
+                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+
             }
         });
     }
