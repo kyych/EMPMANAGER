b1156b9095ff2a835422f986435a5395076ee081
kyych <m.biernat43@gmail.com>
2020-06-15 00:35:09 +0200

    searching for values greather than. Have to implement same thing for values less than some number.

--- src/front/FilterPanel.java
+++ src/front/FilterPanel.java
@@ -7,10 +7,19 @@ import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
 import javax.swing.table.TableRowSorter;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class FilterPanel extends JPanel {
     private final PlaceholderTextField searchForAllTextField = new PlaceholderTextField("");
     private final PlaceholderTextField searchBySalary = new PlaceholderTextField("");
+
+
+    private final JButton statusButton = new JButton("<");  //  default <, on add click will change this value to >, value of salaryButtonLess will change
+    private boolean salaryButtonLess = true;
+
+    private EmployeeTableModel employeeTableModel;
+
 
     private MainViewController controller;
     private TableRowSorter<EmployeeTableModel> tableRowSorter;
@@ -19,9 +28,30 @@ public class FilterPanel extends JPanel {
         this.setLayout(new BorderLayout());
     }
 
+
+    RowFilter<EmployeeTableModel, Integer> GreaterThan = new RowFilter<EmployeeTableModel, Integer>() {
+        public boolean include(Entry<? extends EmployeeTableModel, ? extends Integer> entry) {
+            employeeTableModel = entry.getModel();
+
+            //0 is the first column
+//                if ((int) table.getValueAt(entry.getIdentifier(), 0) > Integer.parseInt(filterText.getText())) {
+//                    return true;
+//                }
+
+            // TODO: works for greaterThan, now implement same thing for lower... :)
+            if ((double) controller.getEmployeeTable().getTable().getValueAt(entry.getIdentifier(), 5) > Double.parseDouble(searchBySalary.getText())) {
+                return true;
+            }
+            return false;
+        }
+    };
+
     public void setUI(){
         Dimension screenDim = Toolkit.getDefaultToolkit().getScreenSize();
         tableRowSorter = new TableRowSorter<>(controller.getEmployeeTable().getTableModel());
+        employeeTableModel = controller.getEmployeeTable().getTableModel();
+
+
 
         controller.getEmployeeTable().getTable().setRowSorter(tableRowSorter);
 
@@ -68,7 +98,8 @@ public class FilterPanel extends JPanel {
                 if (text.trim().length() == 0) {
                     tableRowSorter.setRowFilter(null);
                 } else {
-                    tableRowSorter.setRowFilter(RowFilter.regexFilter("(?i)"+text, 5)); // 5 is salary column
+                    //tableRowSorter.setRowFilter(RowFilter.regexFilter("(?i)"+text, 5)); // 5 is salary column
+                    tableRowSorter.setRowFilter(GreaterThan);
                 }
             }
 
@@ -89,6 +120,17 @@ public class FilterPanel extends JPanel {
 
             }
         });
+
+        statusButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(salaryButtonLess) {statusButton.setText(">");
+                    salaryButtonLess=false;
+                } else {statusButton.setText("<");
+                    salaryButtonLess=true;}
+            }
+        });
+
     }
 
     public void setController(MainViewController mainViewController) {
